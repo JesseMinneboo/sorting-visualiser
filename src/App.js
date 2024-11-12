88ce8e44a37bcb8c27b107a8082fc8a76b4de458
Jesse <47326206+JesseMinneboo@users.noreply.github.com>
2024-11-12 16:12:45 +0100

    Add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import "./index.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
